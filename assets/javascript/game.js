d8d52acc322f0b27f19710d4c0b78fa2a42894e2
el0c <36245622+el0c@users.noreply.github.com>
2018-12-23 02:19:16 -0600

    Fixed bug where getting answers correct would carry over. Addedindicator you got the correct answer and keypress to move forward.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,8 @@ function newGame() {
     $('body').css('background-color', 'blue');
 
     //Display code in hint section
-    $("#answer").text(answer);
+    $('#answer').css('color', 'green');
+    $('#answer').text(answer);
 
     //Reset Vars
     code_guess_array = [];
@@ -28,15 +29,15 @@ function newGame() {
 
     //Display Variables
     $('h1').text("Single Digit Code Breaker!")
-    $("#guessList").text(guesses_string);
-    $("#winCount").text(winCount);
-    $("#lossCount").text(lossCount);
-    $("#triesRemaining").text(triesRemaining);
+    $('#guessList').text(guesses_string);
+    $('#winCount').text(winCount);
+    $('#lossCount').text(lossCount);
+    $('#triesRemaining').text(triesRemaining);
 
 // ---------------------------------------------------------
 // |   		              User Input                	   |
 // ---------------------------------------------------------
-    $(document).on("keypress", event => {
+    $(document).on('keypress', event => {
         //Which key was pressed?
         const userGuess = String.fromCharCode(event.which).toLowerCase();
     
@@ -44,17 +45,29 @@ function newGame() {
         if (/[a-zA-Z0-9-_]/.test(inp)) {
             if (!code_guess_array.includes(userGuess)) {
                 triesRemaining--;
-                $("#triesRemaining").text(triesRemaining);
+                $('#triesRemaining').text(triesRemaining);
                 code_guess_array.push(userGuess);
                 guesses_string += userGuess;
-                $("#guessList").text(guesses_string);
+                $('#guessList').text(guesses_string);
             }
             if (userGuess === answer) {
+                $('body').css('background-color', 'green');
+                $('h1').html('Layer Cracked: <span id="pressKey">Press Space</span> to continue');
+                $('#pressKey').css('color', 'yellow')
+                $('#answer').css('color', 'red');
                 winCount++;
-                newGame();
+                $(document).off('keypress');
+                $(document).on('keypress', function(e) {
+                    if (e.which === 32) {
+                        $(document).off('keypress');
+                        newGame();
+                    }
+                });
             } else if (triesRemaining === 0) {
                 $('body').css('background-color', 'red');
-                $('h1').text("Game Over: Press Space to restart")
+                $('h1').html('Game Over: <span id="pressKey">Press Space</span> to restart');
+                $('#pressKey').css('color', 'yellow');
+                $('#answer').css('color', 'blue');
                 lossCount++;
                 $(document).off("keypress");
                 $(document).on('keypress', function(e) {
